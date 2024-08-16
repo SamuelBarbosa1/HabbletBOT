fa1ff5a2ab7dccfa9de066b6f737c8f3982f265b
SamuelBarbosa1 <samuelbarbosa2001oliveira@gmail.com>
2024-08-16 18:24:59 -0300

    --FIXED BUG BOT

--- buscarHabbo.js
+++ buscarHabbo.js
@@ -42,15 +42,15 @@ const searchUser = args[0];
 const exit = (err) => console.log(err);
 
 const login = async (page) => {
-    await page.waitForTimeout(5000); 
+    await page.waitFor(5000);
     const login = await page.$("input.rounded-input.blue-active.username");
     if (!login) return 0;
     await login.type(userLogin);
 
-    await page.waitForTimeout(1000); 
-    const password = await page.$(`input[name="credentials_password"]`);
+    await page.waitFor(1000);
+    const password = await page.$("input[name='credentials_password']");
     await password.type(userPassword);
-    await page.waitForTimeout(500); 
+    await page.waitFor(500);
 
     await page.click(".rounded-button.blue.plain.submit-form.g-recaptcha.default-prevent");
 }
@@ -69,8 +69,23 @@ const runBot = () => {
             const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36";
             await page.setUserAgent(userAgent);
             await page.setExtraHTTPHeaders({
-                'accept-language': 'en-US,en;q=0.9',
-                'upgrade-insecure-requests': '1'
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+                "accept-encoding": "gzip, deflate, br, zstd",
+                "accept-language": "pt-BR,pt;q=0.6",
+                "cache-control": "max-age=0",
+                "content-type": "application/x-www-form-urlencoded",
+                "origin": "https://www.habblet.city",
+                "referer": "https://www.habblet.city/",
+                "sec-ch-ua": "\"Not)A;Brand\";v=\"99\", \"Brave\";v=\"127\", \"Chromium\";v=\"127\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "same-origin",
+                "sec-fetch-user": "?1",
+                "sec-gpc": "1",
+                "upgrade-insecure-requests": "1",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
             });
 
             await page.evaluateOnNewDocument(() => {
@@ -89,31 +104,31 @@ const runBot = () => {
 
             // Continuar com o login
             await login(page);
-            await page.waitForTimeout(4000); 
+            await page.waitFor(4000);
 
             const game = await browser.newPage();
             await game.goto("https://www.habblet.city/");
             await game.setViewport({ width: 1280, height: 1024 });
 
-            await game.waitForTimeout(2000); 
+            await game.waitFor(2000);
             await game.click("#closeAd1");
-            await game.waitForTimeout(2000);
+            await game.waitFor(2000);
             await game.click("#closeAd2");
 
             await game.waitForSelector(".cursor-pointer.navigation-item.icon.icon-friendsearch");
-            await game.waitForTimeout(2000); 
+            await game.waitFor(2000);
             await game.click(".cursor-pointer.navigation-item.icon.icon-friendsearch");
 
-            await game.waitForTimeout(5000); 
-            const search = await game.$(`input.search-input.goldfish`);
-            await game.waitForTimeout(5000); 
+            await game.waitFor(5000);
+            const search = await game.$("input.search-input.goldfish");
+            await game.waitFor(5000);
             await search.type(searchUser);
-            await game.waitForTimeout(3000); 
+            await game.waitFor(3000);
             await game.click("div.btn-search.goldfish.cursor-pointer");
-            await game.waitForTimeout(4000); 
+            await game.waitFor(4000);
             await game.click("div.cursor-pointer.nitro-friends-spritesheet.icon-profile-sm");
 
-            await game.waitForTimeout(3000); 
+            await game.waitFor(3000);
             const user = {
                 nome: await game.$eval(".d-flex.overflow-hidden.flex-column.gap-2.container-fluid.content-area div.d-inline.text-black.fw-bold", el => el.textContent.trim()),
                 missao: await game.$$eval(".d-flex.overflow-hidden.flex-column.gap-2.container-fluid.content-area div.d-inline.text-black.small", el => el[0].textContent.trim()),
